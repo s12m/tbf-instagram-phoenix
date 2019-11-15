91fbd9678e01879cf7ca511797d200d9c07a0bdc
naito <flower127834.00@gmail.com>
2019-11-16 00:26:30 +0900

    mix phx.gen.html Posts Post posts body:text

---
+++ priv/repo/migrations/20191115145001_create_posts.exs
@@ -0,0 +1,12 @@
+defmodule Instagram.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :body, :text
+
+      timestamps()
+    end
+
+  end
+end
